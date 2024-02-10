fcfee9e8426967c487e03a63130f43938e761bcf
Kaveenxz <kaveenhansithx@gmail.com>
2024-02-10 15:45:41 +0530

    complete searchbar with js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
+// search bar
 
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const itemsContainer = document.getElementById("items-container");
+
+    searchInput.addEventListener("input", function () {
+        const searchQuery = searchInput.value.toLowerCase();
+        const allItems = itemsContainer.querySelectorAll(".arrivels");
+
+        allItems.forEach(function (item) {
+            const itemName = item.querySelector("h5").textContent.toLowerCase();
+            if (itemName.includes(searchQuery)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    });
+});
