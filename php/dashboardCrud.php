7e228c6f260625b7f21ccea83d98b2a94558b596
Kaveenxz <kaveenhansithx@gmail.com>
2024-03-09 11:07:12 +0530

    item crud

---
+++ php/dashboardCrud.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include('./config.php');
+
+if (isset($_POST['add_item'])) {
+    $item_name = mysqli_real_escape_string($conn, $_POST['item_name']);
+    $category = mysqli_real_escape_string($conn, $_POST['category']);
+    $price = $_POST['price'];
+
+    $query = "INSERT INTO items (item_name, category, price) VALUES ('$item_name', '$category', $price)";
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        header("Location: UserDashboard.php");
+        exit();
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+
+if (isset($_POST['update_item'])) {
+    $item_id_update = $_POST['item_id_update'];
+    $item_name_update = mysqli_real_escape_string($conn, $_POST['item_name_update']);
+    $category_update = mysqli_real_escape_string($conn, $_POST['category_update']);
+    $price_update = $_POST['price_update'];
+
+    $query = "UPDATE items SET item_name='$item_name_update', category='$category_update', price=$price_update WHERE item_id=$item_id_update";
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        header("Location: UserDashboard.php");
+        exit();
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+
+if (isset($_POST['delete_item'])) {
+    $item_id_delete = $_POST['item_id_delete'];
+
+    $query = "DELETE FROM items WHERE item_id=$item_id_delete";
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        header("Location: UserDashboard.php");
+        exit();
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+
+mysqli_close($conn);
+?>
